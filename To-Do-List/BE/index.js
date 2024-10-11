bac7d5b5a2742b4d4a9a94b4aa71950bb279c8bd
Mehul0906 <mehulkurhe005@gmail.com>
2024-10-11 17:36:14 +0530

    all

---
+++ To-Do-List/BE/index.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const app=express()
+var cors = require('cors')
+const fs=require("fs")
+app.use(express.json())
+app.use(cors())
+app.get("/getproduct",(req,res)=>{
+    fs.readFile("./db.json","utf-8",(err,data)=>{
+        if(err)
+        {
+          res.send(err)
+        }else{
+            res.send(data)
+        }
+    })
+})
+
+app.listen(8080,()=>{
+    console.log("server is running pn port 8080")
+})
